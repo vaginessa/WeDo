d6a16ddb69fc1363929d1c893d9d513cd79737eb
Nathaniel Clark <pcwizzy37@gmail.com>
2016-06-27 15:53:49 -0700

    Trying to implement a drawing function.

--- app/build/generated/source/r/debug/com/nateapps/wedo/R.java
+++ app/build/generated/source/r/debug/com/nateapps/wedo/R.java
@@ -18,31 +18,78 @@ public final class R {
     public static final class dimen {
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int large_brush=0x7f050002;
+        public static final int medium_brush=0x7f050003;
+        public static final int small_brush=0x7f050004;
     }
     public static final class drawable {
-        public static final int wedo=0x7f020000;
+        public static final int brush=0x7f020000;
+        public static final int eraser=0x7f020001;
+        public static final int large=0x7f020002;
+        public static final int medium=0x7f020003;
+        public static final int new_pic=0x7f020004;
+        public static final int opacity=0x7f020005;
+        public static final int paint=0x7f020006;
+        public static final int paint_pressed=0x7f020007;
+        public static final int pattern1=0x7f020008;
+        public static final int pattern2=0x7f020009;
+        public static final int pattern3=0x7f02000a;
+        public static final int pattern4=0x7f02000b;
+        public static final int pattern5=0x7f02000c;
+        public static final int pattern6=0x7f02000d;
+        public static final int pattern7=0x7f02000e;
+        public static final int pattern8=0x7f02000f;
+        public static final int save=0x7f020010;
+        public static final int small=0x7f020011;
+        public static final int wedo=0x7f020012;
     }
     public static final class id {
-        public static final int butb=0x7f090003;
-        public static final int butbk=0x7f090004;
-        public static final int butg=0x7f090002;
-        public static final int butlevel2=0x7f090005;
-        public static final int butr=0x7f090001;
-        public static final int level1=0x7f090000;
+        public static final int draw_btn=0x7f0a0005;
+        public static final int drawing=0x7f0a0009;
+        public static final int erase_btn=0x7f0a0006;
+        public static final int large_brush=0x7f0a0003;
+        public static final int level1=0x7f0a0000;
+        public static final int medium_brush=0x7f0a0002;
+        public static final int new_btn=0x7f0a0004;
+        public static final int opacity_btn=0x7f0a0007;
+        public static final int opacity_seek=0x7f0a000c;
+        public static final int opq_ok=0x7f0a000d;
+        public static final int opq_txt=0x7f0a000b;
+        public static final int paint_colors=0x7f0a000a;
+        public static final int save_btn=0x7f0a0008;
+        public static final int small_brush=0x7f0a0001;
+    }
+    public static final class integer {
+        public static final int large_size=0x7f070000;
+        public static final int medium_size=0x7f070001;
+        public static final int small_size=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f040000;
-        public static final int level1=0x7f040001;
+        public static final int brush_chooser=0x7f040001;
+        public static final int drawingview=0x7f040002;
+        public static final int opacity_chooser=0x7f040003;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f070000;
+        public static final int action_settings=0x7f080000;
+        public static final int app_name=0x7f080001;
+        public static final int brush=0x7f080002;
+        public static final int erase=0x7f080003;
+        public static final int hello_world=0x7f080004;
+        public static final int lrg=0x7f080005;
+        public static final int med=0x7f080006;
+        public static final int opacity=0x7f080007;
+        public static final int paint=0x7f080008;
+        public static final int save=0x7f080009;
+        public static final int sml=0x7f08000a;
+        public static final int start_new=0x7f08000b;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f080000;
+        public static final int AppTheme=0x7f090000;
     }
 }
